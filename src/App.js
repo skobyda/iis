970eef3b55853a55ebf565def31be93559865967
Simon Kobyda <skobyda@redhat.com>
2019-11-24 14:31:22 +0100

    GUI: Implemented viewing of Tournament, team and player information

--- src/App.js
+++ src/App.js
@@ -4,63 +4,61 @@ import './App.css';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import UserPanel from './userPanel.js';
+import Tournaments from './Tournaments.js';
+import Teams from './Teams.js';
+import Players from './Players.js';
 
-function App() {
-    return (
-        <div className="App">
-            <div>
-                <AppBar position="static">
-                    <Tabs aria-label="simple tabs example">
-                        <Tab label="Tournaments" />
-                        <Tab label="Teams" />
-                        <Tab label="Players" />
-                    </Tabs>
-                </AppBar>
-                <UserPanel />
-            </div>
-            <header className="App-page">
-                <div className="App-content">
-                    <ExpansionPanel>
-                      <ExpansionPanelSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                      >
-                        <Typography>KATOWICE</Typography>
-                      </ExpansionPanelSummary>
-                      <ExpansionPanelDetails>
-                        <Typography>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                          sit amet blandit leo lobortis eget.
-                        </Typography>
-                      </ExpansionPanelDetails>
-                    </ExpansionPanel>
-                    <ExpansionPanel>
-                      <ExpansionPanelSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                      >
-                        <Typography>STEAM Tournament 2021</Typography>
-                      </ExpansionPanelSummary>
-                      <ExpansionPanelDetails>
-                        <Typography>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                          sit amet blandit leo lobortis eget.
-                        </Typography>
-                      </ExpansionPanelDetails>
-                    </ExpansionPanel>
+const menu = { // index of menu items
+    TOURNAMENTS: 0,
+    TEAMS: 1,
+    PLAYERS: 2,
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            page: 0,
+        };
+
+        this.onValueChanged = this.onValueChanged.bind(this);
+    }
+
+    onValueChanged(key, value) {
+        this.setState({ [key]: value });
+    }
+
+    render() {
+        const { page } = this.state;
+
+        return (
+            <div className="App">
+                <div>
+                    <AppBar position="static">
+                        <Tabs aria-label="menu"
+                            onChange={(event, value) => this.onValueChanged("page", value)}
+                            value={page}
+                        >
+                            <Tab label="Tournaments" />
+                            <Tab label="Teams" />
+                            <Tab label="Players" />
+                        </Tabs>
+                    </AppBar>
+                    <UserPanel />
                 </div>
-            </header>
-        </div>
-    );
+                <header className="App-page">
+                    <div className="App-content">
+                        { page === menu.TOURNAMENTS && <Tournaments /> }
+                        { page === menu.TEAMS && <Teams /> }
+                        { page === menu.PLAYERS && <Players /> }
+                    </div>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Players.js
@@ -0,0 +1,97 @@
+import React from 'react';
+
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import './styles.css';
+
+function getPlayers() {
+    // TODO api call to get Players
+
+    // API returns JSON string like this:
+    const playersJSON = '[{"id":"PashaBiceps","fullName":"Jarosław Jarząbkowski","mail":"jaroslav.jarzabkowski@gmail.com","createdAt":"07-11-2017 20:22","countryCode":"PL","born":"11-04-1988","sex":"M","refereeRat":8,"actualPRank":1,"highestPRank":1,"teams":["Ninjas in Pyjamas"]},' +
+                        '{"id":"ZywOo","fullName":"Jarosław Jarząbkowski","mail":"jaroslav.jarzabkowski@gmail.com","createdAt":"07-11-2017 20:22","countryCode":"PL","born":"11-04-1988","sex":"M","refereeRat":8,"actualPRank":1,"highestPRank":1,"teams":["Ninjas in Pyjamas"]},' +
+                        '{"id":"s1mple","fullName":"Jarosław Jarząbkowski","mail":"jaroslav.jarzabkowski@gmail.com","createdAt":"07-11-2017 20:22","countryCode":"PL","born":"11-04-1988","sex":"M","refereeRat":8,"actualPRank":1,"highestPRank":1,"teams":["Ninjas in Pyjamas"]},' +
+                        '{"id":"XANTARES","fullName":"Jarosław Jarząbkowski","mail":"jaroslav.jarzabkowski@gmail.com","createdAt":"07-11-2017 20:22","countryCode":"PL","born":"11-04-1988","sex":"M","refereeRat":8,"actualPRank":1,"highestPRank":1,"teams":["Ninjas in Pyjamas"]},' +
+                        '{"id":"Kaze","fullName":"Jarosław Jarząbkowski","mail":"jaroslav.jarzabkowski@gmail.com","createdAt":"07-11-2017 20:22","countryCode":"PL","born":"11-04-1988","sex":"M","refereeRat":8,"actualPRank":1,"highestPRank":1,"teams":["Ninjas in Pyjamas"]},' +
+                        '{"id":"coldzera","fullName":"Jarosław Jarząbkowski","mail":"jaroslav.jarzabkowski@gmail.com","createdAt":"07-11-2017 20:22","countryCode":"PL","born":"11-04-1988","sex":"M","refereeRat":8,"actualPRank":1,"highestPRank":1,"teams":["Furia Esports"]},' +
+                        '{"id":"BnTeT","fullName":"Jarosław Jarząbkowski","mail":"jaroslav.jarzabkowski@gmail.com","createdAt":"07-11-2017 20:22","countryCode":"PL","born":"11-04-1988","sex":"M","refereeRat":8,"actualPRank":1,"highestPRank":1,"teams":["Furia Esports"]},' +
+                        '{"id":"Sico","fullName":"Jarosław Jarząbkowski","mail":"jaroslav.jarzabkowski@gmail.com","createdAt":"07-11-2017 20:22","countryCode":"PL","born":"11-04-1988","sex":"M","refereeRat":8,"actualPRank":1,"highestPRank":1,"teams":["Furia Esports"]},' +
+                        '{"id":"KSCERATO","fullName":"Jarosław Jarząbkowski","mail":"jaroslav.jarzabkowski@gmail.com","createdAt":"07-11-2017 20:22","countryCode":"PL","born":"11-04-1988","sex":"M","refereeRat":8,"actualPRank":1,"highestPRank":1,"teams":["Furia Esports"]},' +
+                        '{"id":"device","fullName":"Jarosław Jarząbkowski","mail":"jaroslav.jarzabkowski@gmail.com","createdAt":"07-11-2017 20:22","countryCode":"PL","born":"11-04-1988","sex":"M","refereeRat":8,"actualPRank":1,"highestPRank":1,"teams":["Furia Esports"]}]';
+
+    return JSON.parse(playersJSON);
+}
+
+export default class Players extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            players: getPlayers()
+        };
+
+        this.onValueChanged = this.onValueChanged.bind(this);
+    }
+
+    onValueChanged(key, value) {
+        this.setState({ [key]: value });
+    }
+
+    render() {
+        const PlayerPanel = (player) => {
+            const teamsStr = player.teams.join(", ");
+
+            return (
+                <ExpansionPanel key={player.id}>
+                  <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1c-content"
+                    id="panel1c-header"
+                  >
+                      { player.id }
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>
+                      <table id="my-table">
+                        <tbody>
+                            <tr>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                            </tr>
+                            <tr>
+                              <td><b>Name:</b> { player.fullName }</td>
+                              <td><b>Team(s):</b> { teamsStr } </td>
+                            </tr>
+                            <tr>
+                              <td><b>Country:</b> { player.countryCode }</td>
+                              <td><b>Born:</b> { player.born }</td>
+                              <td><b>Sex:</b> { player.sex === "M"
+                                                ? "Male" : "Female" }</td>
+                            </tr>
+                            <tr>
+                              <td><b>Referee rating:</b> { player.refereeRat }</td>
+                              <td><b>Current player rank:</b> { player.actualPRank }</td>
+                              <td><b>Highest player rank:</b> { player.highestPRank }</td>
+                            </tr>
+                        </tbody>
+                      </table>
+                  </ExpansionPanelDetails>
+                  <ExpansionPanelActions>
+                  </ExpansionPanelActions>
+                </ExpansionPanel>
+            );
+        };
+
+        const body = this.state.players.map(player => PlayerPanel(player));
+
+        return(
+            <>
+                { body }
+            </>
+        );
+    };
+}
